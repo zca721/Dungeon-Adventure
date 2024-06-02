46710d14219da5b59f3b41ec46feae6f3cc968ea
Zachary Anderson <zca721@uw.edu>
2024-06-01 19:15:55 -0700

    Added Main Menu to Unity Game

---
+++ Dungeon Adventure/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadScene("Scene 1");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+        Debug.Log("Game Closed");
+    }
+}
