ee05848d3cd9b4ab4de8576c3d6b35b7fb19a168
Zachary Anderson <zca721@uw.edu>
2024-06-03 10:43:35 -0700

    Added Game Over and Restart/Main Menu buttons

---
+++ Dungeon Adventure/Assets/Scripts/Menus/GameOverMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void RestartButton() {
+        SceneManager.LoadScene("Scene 1");
+    }
+
+    public void MainMenuButton() {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
