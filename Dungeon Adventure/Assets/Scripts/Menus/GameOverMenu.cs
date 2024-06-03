4b06bbc75e8a2f39ae64889ca9228a56844b2f88
Zachary Anderson <zca721@uw.edu>
2024-06-03 15:57:05 -0700

    Removed a song and some unneeded code

--- Dungeon Adventure/Assets/Scripts/Menus/GameOverMenu.cs
+++ Dungeon Adventure/Assets/Scripts/Menus/GameOverMenu.cs
@@ -7,19 +7,7 @@ using UnityEngine.SceneManagement;
 public class GameOverMenu : MonoBehaviour
 {
     public void RestartButton() {
-        SceneManager currentScene = new SceneManager();
-
-        // if (SceneManager.ReferenceEquals("Scene 1", "Scene 1")) {
-        //     SceneManager.LoadScene("Scene 1");
-        //     Debug.Log(currentScene.Equals("Scene 1"));
-        //     Debug.Log(SceneManager.ReferenceEquals("Scene 1", "Scene 1"));
-        // } else if (SceneManager.ReferenceEquals("Scene 2", "Scene 2")) {
-        //     SceneManager.LoadScene("Scene 2"); 
-        // }
         SceneManager.LoadScene("Scene 1");
-        // Scene.Equals("Scene 1", "Scene 1");
-        // SceneManager currentScene = new SceneManager();
-        // currentScene.Equals("Scene 1");
     }
 
     public void MainMenuButton() {
